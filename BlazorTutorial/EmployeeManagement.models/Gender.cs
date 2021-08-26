eb97f31749a88234b2131515e39acf74be473692
ctrln01og <henno106@gmail.com>
2021-08-26 20:33:38 +0300

    Models klasside loomine. Töötaja, osakond jms

---
+++ BlazorTutorial/EmployeeManagement.models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.models
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
