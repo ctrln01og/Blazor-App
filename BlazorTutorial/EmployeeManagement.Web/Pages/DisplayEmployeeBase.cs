f391b663fec8c6999ec36ac32c175f795d7cca1d
ctrln01og <henno106@gmail.com>
2021-08-28 01:52:27 +0300

    child to parent

--- BlazorTutorial/EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,22 @@ namespace EmployeeManagement.Web.Pages
         public bool ShowFooter { get; set; }
         [Parameter]
         public EventCallback<bool> OnEmployeeSelection { get; set; }
+
+        [Parameter]
+        public EventCallback<int> OnEmployeeDeleted { get; set; }
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected async Task Delete_Click()
+        {
+            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+            await OnEmployeeDeleted.InvokeAsync(Employee.EmployeeId);
+            //NavigationManager.NavigateTo("/", true);
+        }
 
         protected async Task CheckBoxChanged(ChangeEventArgs e)
         {
